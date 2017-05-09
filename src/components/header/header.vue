d8b2f59687b1d25b8f1541cdfca669469f66b256
2301887641 <sgz19861126>
2017-05-09 18:26:11 +0800

    改为使用styul“

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -49,243 +49,210 @@
   }
 </script>
 
-<style>
-  .header {
-    color: #fff;
-    background: rgba(7, 17, 27, 0.5);
-  }
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
 
-  .header .content-wrapper {
-    padding: 24px 12px 18px 24px;
-    position: relative;
-    /*将父元素的字体大小设为0 取消里面的空白间距*/
-    font-size: 0;
-  }
+  .header
+    position: relative
+    overflow: hidden
+    color: #fff
+    background: rgba(7, 17, 27, 0.5)
+    .content-wrapper
+      position: relative
+      padding: 24px 12px 18px 24px
+      font-size: 0
+      .avatar
+        display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
+      .content
+        display: inline-block
+        margin-left: 16px
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            display: inline-block
+            vertical-align: top
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            background-size: 30px 18px
+            background-repeat: no-repeat
+          .name
+            margin-left: 6px
+            font-size: 16px
+            line-height: 18px
+            font-weight: bold
 
-  .header .avatar {
-    display: inline-block;
-    vertical-align: top;
-  }
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .support
+          .icon
+            display: inline-block
+            vertical-align: top
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height: 12px
+            font-size: 10px
 
-  .header .avatar img {
-    border-radius: 2px;
-  }
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        text-align: center
+        .count
+          vertical-align: top
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height: 24px
+          font-size: 10px
 
-  .header .content {
-    display: inline-block;
-    /*这样才能正确的设置距离左边的间距*/
-    margin-left: 16px;
-  }
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin: 0 4px
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
 
-  .header .content .title {
-    margin: 2px 0 8px 0;
-  }
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      transition: all 0.5s
+      backdrop-filter: blur(10px)
+      &.fade-transition
+        opacity: 1
+        background: rgba(7, 17, 27, 0.8)
+      &.fade-enter, &.fade-leave
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
+      .detail-wrapper
+        width: 100%
+        min-height: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+          .star-wrapper
+            margin-top: 18px
+            padding: 2px 0
+            text-align: center
+          .title
+            display: flex
+            width: 80%
+            margin: 28px auto 24px auto
+            .line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              padding: 0 12px
+              font-weight: 700
+              font-size: 14px
 
-  .header .content .title .brand {
-    display: inline-block;
-    width: 30px;
-    height: 18px;
-    vertical-align: top;
-    background: url("./brand@2x.png");
-    background-size: 30px 18px;
-    background-repeat: no-repeat;
-  }
-
-  .header .content .title .name {
-    margin-left: 6px;
-    font-size: 16px;
-    line-height: 18px;
-    font-weight: bold;
-  }
-
-  body, html {
-    line-height: 1;
-    font-weight: 200;
-    font-family: "PingFang SC", 'STHeitiSc-Light', 'Helvetica-Light', Arial, sans-serif;
-  }
-
-  .header .content .description {
-    margin-bottom: 10px;
-    line-height: 12px;
-    font-size: 12px;
-  }
-
-  .header .content .support .icon {
-    display: inline-block;
-    vertical-align: top;
-    width: 12px;
-    height: 12px;
-    margin-right: 4px;
-    background-size: 12px 12px;
-    background-repeat: no-repeat;
-  }
-
-  .header .content .support .decrease {
-    background: url("./decrease_1@2x.png");
-  }
-
-  .header .content .support .discount {
-    background: url("./discount_1@2x.png");
-  }
-
-  .header .content .support .guarantee {
-    background: url("./guarantee_1@2x.png");
-  }
-
-  .header .content .support .invoice {
-    background: url("./invoice_1@2x.png");
-  }
-
-  .header .content .support .special {
-    background: url("./special_1@2x.png");
-  }
-
-  .header .content .support .text {
-    line-height: 12px;
-    font-size: 10px;
-  }
-
-  .header .content-wrapper .support-count {
-    position: absolute;
-    right: 12px;
-    bottom: 14px;
-    padding: 0 8px;
-    height: 24px;
-    line-height: 24px;
-    border-radius: 14px;
-    background: rgba(0, 0, 0, 0.2);
-    text-align: center;
-  }
-
-  .header .content-wrapper .support-count .count {
-    vertical-align: top;
-    font-size: 10px;
-  }
-
-  .header .content-wrapper .support-count .icon-keyboard_arrow_right {
-    margin-left: 2px;
-    line-height: 24px;
-    font-size: 10px;
-    content: "\e909";
-  }
-
-  .header .bulletin-wrapper {
-    position: relative;
-    height: 28px;
-    line-height: 28px;
-    padding: 0 22px 0 12px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    background: rgba(7, 17, 27, 0.2);
-  }
-
-  .header .bulletin-wrapper .bulletin-title {
-    display: inline-block;
-    vertical-align: top;
-    margin-top: 8px;
-    width: 22px;
-    height: 12px;
-  bg-image('bulletin') background-size: 22 px 12 px;  //------------------
-    background-repeat: no-repeat;
-  }
-
-  .header .bulletin-wrapper .bulletin-title .bulletin-text{
-    vertical-align: top;
-    margin: 0 4px;
-    font-size: 10px;
-  }
-
-  .icon-keyboard_arrow_right{
-    position: absolute;
-    font-size:10px;
-    right:12px;
-    top:8px;
-  }
-
-
-  /*设备像素比为3的*/
-  @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3) {
-    .header .content .title .brand {
-      background: url("./brand@3x.png");
-    }
-  }
-
-  @media (-webkit-min-device-pixel-ratio: 2),(min-device-pixel-ratio: 2) {
-    .header .content .title .brand {
-      background: url("./brand@2x.png");
-    }
-  }
-
-  /*decrease*/
-  @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3) {
-    .header .content .support .decrease {
-      background: url("./decrease_1@3x.png");
-
-    }
-  }
-
-  @media (-webkit-min-device-pixel-ratio: 2),(min-device-pixel-ratio: 2) {
-    .header .content .support .decrease {
-      background: url("./decrease_1@2x.png");
-    }
-  }
-
-  /*discount*/
-  @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3) {
-    .header .content .support .discount {
-      background: url("./discount_1@3x.png");
-
-    }
-  }
-
-  @media (-webkit-min-device-pixel-ratio: 2),(min-device-pixel-ratio: 2) {
-    .header .content .support .discount {
-      background: url("./discount_1@2x.png");
-    }
-  }
-
-  /*guarantee*/
-  @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3) {
-    .header .content .support .guarantee {
-      background: url("./guarantee_1@3x.png");
-
-    }
-  }
-
-  @media (-webkit-min-device-pixel-ratio: 2),(min-device-pixel-ratio: 2) {
-    .header .content .support .guarantee {
-      background: url("./guarantee_1@2x.png");
-    }
-  }
-
-  /*invoice*/
-  @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3) {
-    .header .content .support .invoice {
-      background: url("./invoice_1@3x.png");
-
-    }
-  }
-
-  @media (-webkit-min-device-pixel-ratio: 2),(min-device-pixel-ratio: 2) {
-    .header .content .support .invoice {
-      background: url("./invoice_1@2x.png");
-    }
-  }
-
-  /*special*/
-  @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3) {
-    .header .content .support .special {
-      background: url("./special_1@3x.png");
-
-    }
-  }
-
-  @media (-webkit-min-device-pixel-ratio: 2),(min-device-pixel-ratio: 2) {
-    .header .content .support .special {
-      background: url("./special_1@2x.png");
-    }
-  }
-
-
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height: 16px
+                font-size: 12px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
