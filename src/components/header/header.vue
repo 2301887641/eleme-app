f4a3371e0b2d3174a48cd0aa589e59f55fedb9ae
2301887641 <sgz19861126>
2017-05-18 18:49:25 +0800

    评星

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -22,7 +22,7 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
@@ -30,6 +30,21 @@
     <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div v-show="detailShow" class="detail">
+       <!--stickly footer布局 需要有两个层 1个内容层detail-wrapper 1个底部层-->
+      <div class="detail-wrapper clearfix">
+        <!--真正展示内容的-->
+        <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +55,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+       return {
+           detailShow: false
+       }
+    },
+    methods: {
+      showDetail() {
+          this.detailShow = true
       }
     },
     created() {
@@ -167,92 +192,30 @@
       z-index: -1
       filter: blur(10px)
     .detail
-      position: fixed
-      z-index: 100
-      top: 0
-      left: 0
-      width: 100%
-      height: 100%
-      overflow: auto
-      transition: all 0.5s
-      backdrop-filter: blur(10px)
-      &.fade-transition
-        opacity: 1
-        background: rgba(7, 17, 27, 0.8)
-      &.fade-enter, &.fade-leave
-        opacity: 0
-        background: rgba(7, 17, 27, 0)
+      position:fixed
+      z-index:100
+      width:100%
+      height:100%
+      overflow:auto
+      background:rgba(7,17,27,0.8)
+      top:0
+      left:0
       .detail-wrapper
-        width: 100%
-        min-height: 100%
+        min-height:100%
+        width:100%
         .detail-main
-          margin-top: 64px
-          padding-bottom: 64px
+          margin-top:64px
+          padding-bottom:64px
           .name
             line-height: 16px
-            text-align: center
-            font-size: 16px
-            font-weight: 700
-          .star-wrapper
-            margin-top: 18px
-            padding: 2px 0
-            text-align: center
-          .title
-            display: flex
-            width: 80%
-            margin: 28px auto 24px auto
-            .line
-              flex: 1
-              position: relative
-              top: -6px
-              border-bottom: 1px solid rgba(255, 255, 255, 0.2)
-            .text
-              padding: 0 12px
-              font-weight: 700
-              font-size: 14px
-
-          .supports
-            width: 80%
-            margin: 0 auto
-            .support-item
-              padding: 0 12px
-              margin-bottom: 12px
-              font-size: 0
-              &:last-child
-                margin-bottom: 0
-              .icon
-                display: inline-block
-                width: 16px
-                height: 16px
-                vertical-align: top
-                margin-right: 6px
-                background-size: 16px 16px
-                background-repeat: no-repeat
-                &.decrease
-                  bg-image('decrease_2')
-                &.discount
-                  bg-image('discount_2')
-                &.guarantee
-                  bg-image('guarantee_2')
-                &.invoice
-                  bg-image('invoice_2')
-                &.special
-                  bg-image('special_2')
-              .text
-                line-height: 16px
-                font-size: 12px
-          .bulletin
-            width: 80%
-            margin: 0 auto
-            .content
-              padding: 0 12px
-              line-height: 24px
-              font-size: 12px
+            text-align:center
+            font-size:16px
+            font-weight:700
       .detail-close
-        position: relative
-        width: 32px
-        height: 32px
-        margin: -64px auto 0 auto
-        clear: both
+        position:relative
+        width:32px
+        height:32px
+        margin:-64px auto 0 auto
+        clear:both
         font-size: 32px
 </style>
