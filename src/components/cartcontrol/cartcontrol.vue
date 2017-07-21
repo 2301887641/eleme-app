7bfc56b9586ff5a921768b6efda72719cb8ac3a8
2301887641 <2301887641@qq.com>
2017-07-21 19:03:35 +0800

    完善评价

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="cartcontrol">
     <transition name="move">
-    <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+      <!--@click.stop.prevent 防止点击冒泡 影响其他-->
+    <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart">
       <transition name="inner">
       <span class="inner icon-remove_circle_outline"></span>
       </transition>
     </div>
     </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
-    <div class="cart-add icon-add_circle" @click="addCart"></div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
   </div>
 </template>
 
